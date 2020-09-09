9f772c72629980bb6021a26353d812c4657a4162
Adam Healy <ahealy@2u.com>
2020-09-09 21:06:18 +0200

    Solution for go/hamming: iteration 3

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -9,23 +9,14 @@ import (
 // Distance calculates the hamming distance given to DNA sequences
 func Distance(a, b string) (int, error) {
 	ar, br := []rune(a), []rune(b)
-
 	if len(ar) != len(br) {
-
 		return 0, errors.New("DNA strands are not the same length")
-
 	}
-
 	sum := 0
-
-	for i := 0; i < len(ar); i++ {
-
+	for i := range ar {
 		if ar[i] != br[i] {
 			sum++
 		}
-
 	}
-
 	return sum, nil
-
 }
